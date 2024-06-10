f62321864ca60c102cc9c6064d40275b96fcb5e2
loc1263 <loc1263@hotmail.com>
2024-06-10 17:28:22 -0400

    VueViteTailwindDaisyTemplate

---
+++ README.md
@@ -0,0 +1,12 @@
+# Vue 3 + Vite + Tailwind + Daisy (Template)
+
+"devDependencies": {
+"@vitejs/plugin-vue": "^5.0.4",
+"autoprefixer": "^10.4.19",
+"daisyui": "^4.12.2",
+"postcss": "^8.4.38",
+"tailwindcss": "^3.4.4",
+"vite": "^5.2.0"
+}
+
+## Requeriment
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+    <div>
+        <h1 class="text-cyan-500 text-3xl font-bold underline">Hello world!</h1>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+
+    <div className="p-4">
+        <p className="p-4">aqua theme</p>
+
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-ghost">Ghost</button>
+        <button className="btn btn-link">Link</button>
+    </div>
+</template>
+
+<style></style>
